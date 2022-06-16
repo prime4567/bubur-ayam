b519d02c66fd7264b042cda4ca481be9f65a68ce
YC0SPU <me@benny.id>
2022-06-16 18:30:43 +0700

    Fix post page and slug

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -4,11 +4,11 @@
         <h1 class="flex justify-center font-medium leading-tight text-5xl mt-0 text-blue-600 block">Bubur Ayam</h1>
         <h2 class="flex justify-center font-medium leading-tight text-2xl mt-0 text-blue-600 block">#timbuburdiaduk</h2>
     </header>
-    <div class="container mx-auto pt-16 lg:pt-32">
+    <div class="container mx-auto pt-16 lg:pt-32 px-2 md:px-16 lg:px-32 xl:px-64">
       <NuxtLink :to="article.slug">
         <div>
-          <nuxt-img :src="article.cover_image" sizes="sm:100vw md:50vw lg:600px"/>
-          <h3 class="font-medium text-2xl mt-2">{{ article.title }}</h3>
+          <nuxt-img :src="article.cover_image" sizes="sm:100vw md:100vw lg:100vw" />
+          <h3 class="font-medium text-2xl mt-2 mb-4">{{ article.title }}</h3>
           <nuxt-content :document="article" />
         </div>
       </NuxtLink>
