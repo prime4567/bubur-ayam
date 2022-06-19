ce0154a29b325b3c65ec06b928877fdd07299313
YC0SPU <me@benny.id>
2022-06-19 22:31:59 +0700

    Fix layout front page

--- pages/index.vue
+++ pages/index.vue
@@ -5,16 +5,16 @@
         <h2 class="flex justify-center font-medium leading-tight text-2xl mt-0 text-blue-600 block">#timbuburdiaduk</h2>
     </header>
     <div class="container mx-auto pt-16 px-2 lg:pt-32">
-      <div class="grid lg:grid-rows-3 lg:grid-flow-col">
+      <h3 class="font-medium text-3xl mt-2 mb-4">Latest Post</h3>
+      <div class="grid lg:grid-rows-3 lg:grid-flow-col lg:gap-x-8 gap-y-4">
         <div :class="getCols(index)" v-for="(blog, index) of blogs" :key="blog.slug">
           <NuxtLink :to="blog.slug">
             <div v-if="index===0">
-              <h3 class="font-medium text-3xl mt-2 mb-4">Latest Post</h3>
               <nuxt-img :src="blog.cover_image" sizes="sm:100vw lg:600px"/>
               <h4 class="font-medium text-2xl mt-2">{{ blog.title }}</h4>
             </div>
-            <div v-else>
-              <nuxt-img :src="blog.cover_image" sizes="sm:100vw md:100vw lg:200px"/>
+            <div class="lg:flex" v-else>
+              <nuxt-img :src="blog.cover_image" sizes="sm:100vw md:100vw lg:200px" class="lg:pr-4"/>
               <h4 class="font-medium text-2xl mt-2">{{ blog.title }}</h4>
             </div>
           </NuxtLink>
