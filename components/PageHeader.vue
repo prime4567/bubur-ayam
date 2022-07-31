32850fb6440955c51f03a59c47e6268f0755c020
YC0SPU <me@benny.id>
2022-07-31 16:21:48 +0700

    Refactor star color

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -1,6 +1,8 @@
 <template>
     <header>
-        <nuxt-img class="flex mx-auto mt-8" src="/img/logo-bubur-ayam.png" alt="Logo Bubur Ayam #timbuburdiaduk" />
+        <NuxtLink to="/">
+            <nuxt-img class="flex mx-auto mt-8" src="/img/logo-bubur-ayam.png" alt="Logo Bubur Ayam #timbuburdiaduk" />
+        </NuxtLink>
         <!--
         <h1 class="flex justify-center font-medium leading-tight text-5xl mt-0 text-blue-600 block">Bubur Ayam</h1>
         <h2 class="flex justify-center font-medium leading-tight text-2xl mt-0 text-blue-600 block">#timbuburdiaduk</h2>
--- components/StarRating.vue
+++ components/StarRating.vue
@@ -3,13 +3,13 @@
         <svg v-for="index in 5" :key=index version="1.1" xmlns="http://www.w3.org/2000/svg"  width="21px" viewBox="0 0 50 50">
             <defs>
                 <linearGradient id="half" x1="0" x2="100%" y1="0" y2="0">
-                    <stop offset="50%" stop-color="#FFD700"></stop>
-                    <stop offset="50%" stop-color="#D3D3D3"></stop>
+                    <stop offset="50%" stop-color="var(--star-color-filled)"></stop>
+                    <stop offset="50%" stop-color="var(--star-color-blank)"></stop>
                 </linearGradient>
             </defs>
-            <path v-if="(index*2)<=rating" class="star-svg-full" d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z"/>
+            <path v-if="(index*2)<=rating" fill="var(--star-color-filled)" d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z"/>
             <path v-else-if="(index*2-rating)==1" fill="url(#half)" d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z"/>
-            <path v-else class="star-svg-blank" d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z"/>
+            <path v-else fill="var(--star-color-blank)" d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z"/>
         </svg>
         {{ rating }}/10
     </div>
